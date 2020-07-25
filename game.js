ea39f4582e97a32dedd3435a82bfb654d6bacb4f
Meerxn <fardeenfmeeran@gmail.com>
2020-07-25 21:35:36 +0400

    user clicks and sounds added

--- game.js
+++ game.js
@@ -1,10 +1,27 @@
 var pattern  = [];
+var usrPattern = []; 
 var  buttons = new Array("red", "blue", "green","yellow");
 function nextSequence(){
     var randNum = Math.floor(Math.random()*(4)); 
     var randColor = buttons[nextSequence()];
     pattern.push(randColor);
+    $("#" + "randColor").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound();
+
   }
+  $(".btn").click(function(){
+
+  var userColor = (this.id);
+  usrPattern.push(userColor);
+  playSound(userColor);
+  })
+  function playSound(color){
+    var aud = new Audio("sounds/" + color + ".mp3" );
+    aud.play();
+  }
+  
+
+
 
 
 
