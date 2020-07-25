7018db5cbe08c9f6cd54a249af84f50f5ee79f33
Meerxn <fardeenfmeeran@gmail.com>
2020-07-25 21:04:15 +0400

    step 2 added random generator

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-alert("This is the simon game");
+var pattern  = [];
+var  buttons = new Array("red", "blue", "green","yellow");
+function nextSequence(){
+    var randNum = Math.floor(Math.random()*(4)); 
+    var randColor = buttons[nextSequence()];
+    pattern.push(randColor);
+  }
+
+
+
+
+
+
+
+
